bee1c28ab7aeca6399d4d75fed47dfbfef51fa48
Pius <pius.dev@pm.me>
2023-08-24 07:22:59 +0000

    Done

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use std::time::Duration;
+use std::time::Instant;
+use rand::Rng;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, challanger!");
+    println!("When you read the word Rust, press quickly enter");
+    println!("Your reflex time will be shown afterwards.");
+   
+    let rand_sec = rand::thread_rng()
+        .gen_range(5..10);
+    let rand_nano = rand::thread_rng()
+        .gen_range(1..999);
+    let rand_dur = Duration::new(rand_sec, rand_nano);
+    let wait = Instant::now();
+    while wait.elapsed() < rand_dur {
+    }
+    println!("Rust");
+    let rflx_time = Instant::now();
+    rflx_action();
+    let elapsed_time = rflx_time.elapsed();
+    println!("Your reflex time is: {:?}" , elapsed_time);
+}
+
+fn rflx_action(){
+    let mut enter = String::new(); 
+    io::stdin()
+        .read_line(&mut enter)
+        .expect("Failed to read line");
 }
